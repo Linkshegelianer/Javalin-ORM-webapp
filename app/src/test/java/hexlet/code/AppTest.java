028ea8a5aa5f99ea2a1e639afa98357aeba0ce75
Lera <mssolipsismus@gmail.com>
2023-05-22 16:25:51 +0300

    add tests

---
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,71 @@
+package hexlet.code;
+
+
+import hexlet.code.domain.Url;
+import io.ebean.DB;
+import io.ebean.Database;
+import io.javalin.Javalin;
+import org.junit.jupiter.api.*;
+
+
+import kong.unirest.HttpResponse;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AppTest {
+
+    @Test
+    void testInit() {
+        assertThat(true).isEqualTo(true);
+    }
+
+    private static Javalin app;
+    private static String baseUrl;
+    private static Url existingUrl;
+    private static Database database;
+
+    @BeforeAll
+    public static void beforeAll() {
+        app = App.getApp();
+        app.start(0);
+        int port = app.port();
+        baseUrl = "http://localhost:" + port;
+        database = DB.getDefault();
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        app.stop();
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        database.script().run("/truncate.sql");
+        database.script().run("/seed-test-db.sql");
+    }
+
+    @Test
+    void testIndex() {
+        HttpResponse<String> response = Unirest.get(baseUrl).asString();
+        assertThat(response.getStatus()).isEqualTo(200);
+        assertThat(response.getBody()).contains("Анализатор страниц");
+    }
+
+    @Nested
+    class UrlTest() {
+
+        @Test
+        void testIndex() {
+            HttpResponse<String> response = Unirest
+                    .get(baseUrl + "/urls")
+                    .asString();
+            String body = response.getBody();
+
+            assertThat(response.getStatus()).isEqualTo(200);
+            assertThat(body).contains("The Man Within");
+            assertThat(body).contains("Consider the Lilies");
+        }
+    }
+
+}
