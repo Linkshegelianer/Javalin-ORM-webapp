d218c20b7361f421c682f623c3aea224e89e9e60
Lera <mssolipsismus@gmail.com>
2023-06-14 15:42:42 +0300

    refactor for autotests

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -17,7 +17,6 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 
-import javax.persistence.PersistenceException;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -55,18 +54,15 @@ class AppTest {
         database = DB.getDefault();
 
         mockWebServer = new MockWebServer();
-        try {
-            mockWebServer.start();
-        } catch (PersistenceException e) {
-            System.out.println("Something with the connection");
-        }
-
+        mockWebServer.start();
     }
 
     @AfterAll
     public static void afterAll() throws IOException {
+        database.script().run("/truncate.sql");
         app.stop();
         mockWebServer.shutdown();
+
     }
 
     @BeforeEach
