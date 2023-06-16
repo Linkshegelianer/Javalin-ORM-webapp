e31f425c603de2af7d2dda3f4e8c0abcb22bbba9
Lera <mssolipsismus@gmail.com>
2023-06-16 21:36:56 +0300

    fix bootstrap

--- app/src/main/java/hexlet/code/domain/UrlCheck.java
+++ app/src/main/java/hexlet/code/domain/UrlCheck.java
@@ -8,9 +8,6 @@ import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 
 @Entity
 public final class UrlCheck extends Model {
@@ -62,8 +59,6 @@ public final class UrlCheck extends Model {
     }
 
     public Instant getCreatedAt() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-        LocalDateTime dateTime = LocalDateTime.ofInstant(this.createdAt, ZoneId.systemDefault());
         return createdAt;
     }
 
